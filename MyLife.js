83ca4b4ed5d38f87d682897b73b3a016196987c5
sonpanabtej <hv2471920@gmail.com>
2022-09-30 10:53:26 +0530

    Create MyLife.js

---
+++ MyLife.js
@@ -0,0 +1,60 @@
+class MyLife
+{
+     constructor(a,b)
+     {
+         a = this.a;
+         b = this.b;
+     }
+     Chap1()
+     {
+       return "SWFtIEhhcnNoYSBWYXJkaGFuIE9uZSBEYXkgVHVlc2RheSBNeSBMaWZlIFR1cm5lZCBCZWNhdXNlIG9mIG15IHNpc3RlciAgKFRoYXNsZWVtKQ==";
+     }
+     Chap2()
+     {
+       return "VGhhdCBEYXkgV2UgQ29udmVyc2F0aW9uIGluIEdyb3VwIERpc2N1c3Npb24u";
+     }
+     Chap3()
+     {
+       return "U2hlIENhbGxlZCBtZSBiaGFpeWEoYnJvdGhlcikgT24gdGhhdCAybmQgZGF5IGZyb20gdGhlbiBvbndhcmRzIFNoZSBpcyBNeSBTaXN0ZXIgRm9yZXZlciBJIExvdmUgbXkgc2lzdGVyIHZlcnkgTXVjaC4=";
+     }
+     Chap4()
+     {
+       return "QnV0IFN1ZGRlbmx5IG91ciBjb252ZXJzYXRpb24gY3V0dGVkIHdoeSBJIGFsc28gZG9uJ3Qga25vdy4=";
+     }
+     Chap5()
+     {
+       return "V2hhdCBpIGRpZCB3cm9uZyBpIGFsc28gZG9uJ3Qga25vdz8=";
+     }
+     Chap6()
+     {
+       return "V2h5IHNoZSBpcyBub3QgVGFsa2luZyB0byBicm90aGVyP0luIG15IFRvdGFsIGxpZmUgSSBDcmllZCBmb3IgSGVyIE5vIE90aGVyIGdpcmwgSSB3YW50IHRvIHRhbGsgYW5kIGNoYXQgd2l0aCBoZXI=";
+     }
+     Chap7()
+     {
+       return "T25lIGRheSBJIE1lc3NhZ2VkIGhlciBvbiBKdWx5IDIyIDIwMjIu";
+     }
+     Chap8()
+     {
+       return "T24gTmV4dCBkYXkgRnJpZGF5IFNoZSBTYWlkIE5vdCB0byBtZXNzYWdlIGFnYWluLkkgc2FpZCBvayBhbmQgSSBDcmllZCBMb3QgTG90IExvdCBMb3QgTG90IExvdC4uLi4uLi4uLi4uLi4uLi4=";
+     }
+     Chap9()
+     {
+       return "RnJvbSB0aGF0IGRheSBvdXIgY29udmVyc2F0aW9uIGN1dHRlZCBzaGUgYXZvaWRzIG1lIGFueSB3YXkgU2hlIHNob3VsZCBiZSBzYWZlIHdoZXJlIGV2ZXIgc2hlIHNob3VsZCBsaXZlIGhhcHB5LiBNeSBMb3ZlbHkgc2lzdGVyLg==";
+     }
+     Chap10()
+     {
+       return "SSB3YW50IHRvIHRhbGsgdG8geW91IHNpc3RlciBidXQgd2l0aCB3aGljaCB1Z2x5IGZhY2UgIGkgc2hvdWxkIHRhbGsgdG8geW91Py4=";
+     }
+     Chap11()
+     {
+       return "VGhhbmt5b3UgZm9yIGV2ZXJ5dGhpbmcgbXkgc2lzdGVyIGxvbmcgbGlmZSBtb3JlIHRoYW4gMTAwIHllYXJzIEhhcHBpbHkgVGhhdCdzIG15IFdpc2ggeW91IHRhbGsgdG8gbWUgb3Igbm90IHRoYXQgaXMgeW91ciB3aXNoIGJ1dCBiZSBzYWZlLg==";
+     }
+     Chap12()
+     {
+       return "SWYgVGhpcyBJcyBzZWVuIGJ5IFlvdSBPayBJYW0gaGFwcHkuTXkgTGlmZSBpcyBteSBzaXN0ZXIgb25seS4=";
+     }
+     Chap13()
+     {
+       return "IEkgVGhvdWdodCBUaGVyZSBpcyBhIFNpc3RlciBmb3IgbWUgSSBjYW4gU2hhcmUgbXkgYWxsIHByb2JsZW1zIGJ1dCBpdCBmYWlsZWQgdG8gc2hhcmUgbXkgcHJvYmxlbXMgdG8gbXkgc2lzdGVyLg==";
+     }
+}
