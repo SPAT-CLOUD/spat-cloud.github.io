150b21d2aa726b42a51bc3ce792e8b456b6002aa
sonpanabtej <hv2471920@gmail.com>
2022-10-12 07:32:52 +0530

    Create MPrivate.js

---
+++ MPrivate.js
@@ -0,0 +1,83 @@
+export default class Secrect
+{
+   constructor(x)
+   {
+     x = this.x;
+   }
+   setKey()
+   {
+      return "TWVFbFNhSHQ"
+   }
+   partA()
+   {
+      return "SWFTWVFbFNhSHQIEhhcnNoYSBWYXJkaGFuIE9uZSBEYXkgVHVlc2RheSBNeSBMaWZlIFR1cm5lZCBCZWNhdXNlIG9mIG15IHNpc3RlciAgKFRoYXNsZWVTWVFbFNhSHQKQ";
+   }
+   partB()
+   {
+      return "VGTWVFbFNhSHQTWVFbFNhSHQdCBEYXkgV2UgQ29udmVyc2F0aW9uIGluIEdyb3VwIERpc2N1c3Npb24u";
+   }
+   partC()
+   {
+      return "U2hlIENhbGxlZCBtZSBiTWVFbFNhSHQGFpeWEoYnJvdGhlcikgT24gdGhhdCAybmQgZGF5IGZyb20gdGhlbiBvbndhcmRzIFNoZSBpcyBNeSBTTWVFbFNhSHQXN0ZXIgRm9yZXZlciBJIExvdmUgbXkgc2lzdGVyIHZlcnkgTXVjTWVFbFNhSHQC4";
+   }
+   partD()
+   {
+      return "QnV0IFN1ZGRlbmx5IG91ciBjb252ZXJzYXRpb24gY3V0dGVkIHdoeTWVFbFNhSHQBJIGFsc28gZG9uJ3Qga25vdy4";
+   }
+   partE()
+   {
+      return "V2hhdCBpIGRpZCB3cm9uZyBpIGFsc28gZG9uJ3Qga25vdz8";
+   }
+   partF()
+   {
+      return "V2h5IHNoZSBpcyBub3QgVGFsa2luZyB0byBicm90aGVyP0luIG15IFRvdGFsIGxpZmUgSSBDcmllZCBmb3IgSGVyITWVFbFNhSHQ5vITWVFbFNhSHQ90aGVyIGdpcmwgSSB3YW50IHRvIHRhbGsgYW5kIGNoYXQgd2l0aCBoZXI";
+   }
+   partG()
+   {
+      return "T25lIGRhTWVFbFNhSHQSBJIE1lc3NhZ2VkIGhlciBvbiBKdWx5IDIyIDIwMjIu";
+   }
+   partH()
+   {
+      return "T24gTmV4dCBkYXkgRnJpZGF5IFNoZSBTYWlkIE5vdCB0byBtZXNzYWdlIGFnYWluLkkgc2FpZCBvayBhbmQgSSBDcmllZCBTWVFbFNhSHQb3QgTG90IExvdCBTWVFbFNhSHQb3QgTG90IExvdC4uLi4uLi4uLi4uLi4uLi4";
+   }
+   partI()
+   {
+      return "RnJvbSB0aGF0IGRheSBvdXIgY29udmVyc2F0aW9uIGN1dHRlZCBzaGUgYXZvaWRzIG1lIGFueSB3YXkgU2hlIHNob3VsZCBiZSBzYWZlIHdoZXJlIGV2ZXIgc2hlIHNob3VsZCBsaXZlIGhhcHB5LiBNeSBMb3ZlbHkgc2lzdGVyLg";
+   }
+   partJ()
+   {
+      return "SSB3YW50IHRvIHRTWVFbFNhSHQbGsgdG8geW91IHNpc3RlciBidXQgd2l0aCB3aGljaCB1Z2x5IGZTWVFbFNhSHQY2UgIGkgc2TWVFbFNhSHQvdWxkIHRTWVFbFNhSHQbGsgdG8geW91Py4";
+   }
+   partK()
+   {
+      return "VGhhbmt5b3UgZm9yIGV2ZXJ5dGhpbmcgbXkgc2lzdGVyIGxvbmcgbGlmZSBtb3JlIHRoYW4gMTAwIHllYXJzIEhhcHBpbHkgVGhhdCdzIG15IFdpc2ggeW91IHRhbGsgdG8gbWUgb3Igbm90IHRoYXQgTWVFbFNhSHQXMgeW91ciB3TWVFbFNhSHQXNoIGJ1dCBiZSBzYWZlLg";
+   }
+   partL()
+   {
+      return "TWVFbFNhSHQWYgVGhpcyBJcyBzZWVuIGJ5IFlvdTWVFbFNhSHQBPayBJYW0gaGFwcHkuTXkgTGlmZTWVFbFNhSHQBpcyBteTWVFbFNhSHQBzaXN0ZXIgb25seTWVFbFNhSHQ4";
+   }
+   partM()
+   {
+      return "IEkgVGhvdWdodCBUaGVyZSBpcyBhIFNpc3RTWVFbFNhSHQciBmb3IgbWUgSSBjYW4gU2hhcmUgbXkgYWxsIHByb2JsZW1zIGJ1dCBpdCBmYWTWVFbFNhSHQsZWQgdG8gc2hhcmUgbXkgcHJvYmxTWVFbFNhSHQbXMgdG8gbXkgc2TWVFbFNhSHQzdGVyLg";
+   }
+   Aerror()
+   {
+     return window.atob(this.partA().split(this.setKey()).join('On'));
+   }
+   Berror()
+   {
+     return window.atob(this.partB().split(this.setKey()).join('OnETuE'));
+   }
+   Jerror()
+   {
+     return window.atob(this.partJ().split(this.setKey()).join('wItHoUtHeRiNoTaBlEtOlIvE'));
+   }
+   Lerror()
+   {
+     return window.atob(this.partL().split(this.setKey()).join('mYsIsTeRaDdMyLiFeTiMeAlSo'));
+   }
+   Merror()
+   {
+     return window.atob(this.partM().split(this.setKey()).join('mEwIlLdOaNyThInGfOrHeRsHeShOuLdBeSaFeAnDhApPyHeRpRoBlEmSaNdPaInShOuLdBeMiNesHeShOuLdBeHaPpYiSwIsHoFYoUrBrOThErHaRsHa'));
+   }
+}
