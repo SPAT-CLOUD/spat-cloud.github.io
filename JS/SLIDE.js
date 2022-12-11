f5a4386ce2782ac1e2cf55105a0df2c40573cacb
sonpanabtej <hv2471920@gmail.com>
2022-12-11 19:01:55 +0530

    Create SLIDE.js

---
+++ JS/SLIDE.js
@@ -0,0 +1,125 @@
+var t = {'s':function(d){$HShow(d);},'h':function(g){$HVanish(g);}};
+function sw(x)
+{
+switch(x)
+{
+case 1:
+t.s('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 2:
+t.h('S1');t.s('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 3:
+t.h('S1');t.h('S2');t.s('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 4:
+t.h('S1');t.h('S2');t.h('S3');t.s('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 5:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.s('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 6:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.s('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 7:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.s('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 8:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.s('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 9:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.s('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 10:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 11:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.s('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 12:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.s('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 13:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.s('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 14:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.s('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 15:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.s('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 16:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.s('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 17:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.s('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 18:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.s('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 19:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.s('S19');t.h('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 20:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.s('S20');t.h('S21');t.h('S22');t.h('S23');
+break;
+case 21:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.s('S21');t.h('S22');t.h('S23');
+break;
+case 22:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.s('S22');t.h('S23');
+break;
+case 23:
+t.h('S1');t.h('S2');t.h('S3');t.h('S4');t.h('S5');
+t.h('S6');t.h('S7');t.h('S8');t.h('S9');t.h('S10');
+t.h('S11');t.h('S12');t.h('S13');t.h('S14');t.h('S15');t.h('S16');t.h('S17');t.h('S18');t.h('S19');t.h('S20');t.h('S21');t.h('S22');t.s('S23');
+break;
+default:console.log('ImgNotFound');
+}
+}
+window.addEventListener("contextmenu",function(e){e.preventDefault();},false);
+setInterval(()=>sw(Math.floor(Math.random()*23)),2000);
