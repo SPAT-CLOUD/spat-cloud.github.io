7bc187854c7043167f6529f963d54c207dfb7e25
sonpanabtej <hv2471920@gmail.com>
2022-12-18 07:34:45 +0530

    Create Key.js

---
+++ JS/Key.js
@@ -0,0 +1,14 @@
+export default WishOfUglyFellow class
+{
+constructor(x)
+{
+x = this.x;
+}
+show(){
+var t = new XMLHttpRequest();
+var es = "";
+t.onload=function(){
+es +=this.responseText;
+}
+return es;
+}
