c842203245fec8e385e2999e2d4bc3eef0cc5069
sonpanabtej <hv2471920@gmail.com>
2023-01-26 22:28:15 +0530

    Update OnlineAudio.js

--- OnlineAudio.js
+++ OnlineAudio.js
@@ -1,4 +1,4 @@
-var Audio = {
+var ogg = {
      "Movies":
       {
              "Lang":
@@ -6,30 +6,40 @@ var Audio = {
                 "Telugu":
                  {
                     "RangDe":{
-        "EmithoIdhi":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2001%20-%20Emito%20Idhi.mp3",
-"BusStande":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2002%20-%20Bus%20Stande%20Bus%20Stande.mp3",
-"NaaKanulu":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2003%20-%20Naa%20Kanulu%20Yepudu.mp3",
-"Rangule":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2005%20-%20Rangule.mp3",
-"Ooranta":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2006%20-%20Oorantha.mp3",
-"ChoosiNerchukoku":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2004%20-%20Choosi%20Nerchukoku.mp3"
-}
-},
-        "Tamil":
-         {
-      "Beast":{"ArabicKuthu":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Arabic%20Kuthu.mp3",
-"JollyOGymkhana":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Jolly%20O%20Gymkhana.mp3",
-"BeastMode":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Beast%20Mode.mp3",
-},
-"Kanchana":{"KarrupuPerazhaga":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Karuppu%20Perazhaga.mp3",
-"NilluNillu":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Nillu%20Nillu%20Nillu%20Nillu.mp3",
-"SangliBungli":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Sangili%20Bungili%20-%20Masstamilan.in.mp3",
-"KodiyavaninKathya":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Kodiyavanin%20Kathaya.mp3"
-},
-"Enthiran":{"ArimaArima":"https://naasongs.vip/links/uploads/Enthiran%2520/Arima-Arima.mp3",
-"BoomBoomRoboDa":"https://naasongs.vip/links/uploads/Enthiran%2520/Boom-Boom-Robo-Da.mp3"},
-"KGF1":{"DheeraDheera":"https://friendstamilmp3.net/Filesystem/A-Z%20Movie%20Songs/KGF%20Chapter%201/Agni%20Nenjil%20(Dheera%20Dheera%20Minnal%20).mp3"},
-"KGF2":{"Toofan":"https://naasongs.vip/myuploads/uploads/KGF%202/Toofan%20%20Tamil.mp3"},
-"NaaneVaruvean":{"VeeraSoora":"https://pagalnew.com/download128/33103"}
+                             "EmithoIdhi":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2001%20-%20Emito%20Idhi.mp3",
+                             "BusStande":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2002%20-%20Bus%20Stande%20Bus%20Stande.mp3",
+                             "NaaKanulu":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2003%20-%20Naa%20Kanulu%20Yepudu.mp3",
+                             "Rangule":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2005%20-%20Rangule.mp3",
+                             "Ooranta":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2006%20-%20Oorantha.mp3",
+                             "ChoosiNerchukoku":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2004%20-%20Choosi%20Nerchukoku.mp3"
+                             }
+                 },
+               "Tamil":
+                {
+                    "Beast":{
+                            "ArabicKuthu":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Arabic%20Kuthu.mp3",
+                            "JollyOGymkhana":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Jolly%20O%20Gymkhana.mp3",
+                            "BeastMode":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Beast%20Mode.mp3",
+                            },
+                   "Kanchana":{
+                            "KarrupuPerazhaga":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Karuppu%20Perazhaga.mp3",
+                            "NilluNillu":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Nillu%20Nillu%20Nillu%20Nillu.mp3",
+                            "SangliBungli":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Sangili%20Bungili%20-%20Masstamilan.in.mp3",
+                            "KodiyavaninKathya":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Kodiyavanin%20Kathaya.mp3"
+                           },
+                  "Enthiran":{
+                            "ArimaArima":"https://naasongs.vip/links/uploads/Enthiran%2520/Arima-Arima.mp3",
+                            "BoomBoomRoboDa":"https://naasongs.vip/links/uploads/Enthiran%2520/Boom-Boom-Robo-Da.mp3"
+                          },
+                  "KGF1":{
+                            "DheeraDheera":"https://friendstamilmp3.net/Filesystem/A-Z%20Movie%20Songs/KGF%20Chapter%201/Agni%20Nenjil%20(Dheera%20Dheera%20Minnal%20).mp3"
+                         },
+                  "KGF2":{
+                            "Toofan":"https://naasongs.vip/myuploads/uploads/KGF%202/Toofan%20%20Tamil.mp3"
+                         },
+                  "NaaneVaruvean":{
+                             "VeeraSoora":"https://pagalnew.com/download128/33103"
+                                  }
          }
        }
     }
