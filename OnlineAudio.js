32fd77020f291171c68a949b9ac7c24fb4d142f4
sonpanabtej <hv2471920@gmail.com>
2023-01-28 10:37:12 +0530

    Update OnlineAudio.js

--- OnlineAudio.js
+++ OnlineAudio.js
@@ -1,44 +1,99 @@
+/*
+ * TEMPLATE
+
+"NN":
+{
+"title":"",
+"URL":""
+},
+
+Movies
+     |
+     |___Lang
+            |
+            |___Telugu
+                     |
+                     |____RangDe
+                     |         |
+                     |         |___Emitho Idhi
+                     |         |___Bus Stande
+                     |         |___Na Kanulu Eppudu
+                     |         |___Rangule
+                     |         |___Ooranta Vennela
+                     |         |___Choosi Nerchukoku
+                     |
+                     |____Bimbisara
+                     |            |___Eeswarude
+                     |            |___Gulebakavali
+                     |            |___O Tene Palukula
+                     |            |___Neetho Unthe Chalu
+                     |            |___Vijay Ho
+                     |            |___Bimbisara Rap Song
+                     |____Sulthan
+                     |          |__Andhame Athivai
+                     |          |__Jai Sulthan
+
+                               
+
+
+*/
+
 var ogg = {
      "Movies":
       {
-             "Lang":
+       "Lang":
+        {
+         "Telugu":
+          {
+             "RangDe":
              {
-                "Telugu":
-                 {
-                    "RangDe":{
-                             "EmithoIdhi":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2001%20-%20Emito%20Idhi.mp3",
-                             "BusStande":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2002%20-%20Bus%20Stande%20Bus%20Stande.mp3",
-                             "NaaKanulu":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2003%20-%20Naa%20Kanulu%20Yepudu.mp3",
-                             "Rangule":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2005%20-%20Rangule.mp3",
-                             "Ooranta":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2006%20-%20Oorantha.mp3",
-                             "ChoosiNerchukoku":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2004%20-%20Choosi%20Nerchukoku.mp3"
-                             }
-                 },
+               "EI":{"URL":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2001%20-%20Emito%20Idhi.mp3","title":"Emitho Idhi"},
+               "BS":{"title":"Bus Stande Bus Stande","URL":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2002%20-%20Bus%20Stande%20Bus%20Stande.mp3"},
+               "NKY":{"title":"NaaKanulu","URL":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2003%20-%20Naa%20Kanulu%20Yepudu.mp3"},
+               "Rangule":{"title":"Rangule","URL":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2005%20-%20Rangule.mp3"},
+               "OV":{"title":"Ooranta Vennala","URL":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2006%20-%20Oorantha.mp3"},
+               "CN":{"title":"ChoosiNerchukoku","URL":"https://s088.naasongs.download/Rang%20De%20-%20(2021)/[iSongs.info]%2004%20-%20Choosi%20Nerchukoku.mp3"}
+             },
+            "Bimbisara":
+             {
+               "ES":{"title":"Eeswarude","URL":"https://s093.naasongs.download/Bimbisara%20-%20(2022)/[iSongs.info]%2001%20-%20Eeshwarude.mp3"},
+               "GK":{"title":"Gulebakavali","URL":"https://s093.naasongs.download/Bimbisara%20-%20(2022)/[iSongs.info]%2005%20-%20Gulebakavali.mp3"},
+               "OTP":{"title":"O Tene Palukula","URL":"https://s093.naasongs.download/Bimbisara%20-%20(2022)/[iSongs.info]%2002%20-%20O%20Tene%20Palukula.mp3"},
+               "NUC":{"title":"Netho Unte Chalu","URL":"https://s093.naasongs.download/Bimbisara%20-%20(2022)/[iSongs.info]%2003%20-%20Neetho%20Unte%20Chalu.mp3"},
+               "VH":{"title":"Vijaya Ho","URL":"https://s093.naasongs.download/Bimbisara%20-%20(2022)/[iSongs.info]%2004%20-%20Vijayaho.mp3"},
+               "BS_EN":{"title":"Bimbisara Rap Song","URL":"https://s093.naasongs.download/Bimbisara%20-%20(2022)/[iSongs.info]%2006%20-%20Bimbi%20Bimbisara.mp3"}
+            },
+            "Sulthan":
+            {
+                "AA":{"title":"Andhame Athivai","URL":"https://s089.naasongs.download/Sultan%20-%20(2021)/[iSongs.info]%2002%20-%20Andhame%20Athivai%20Vasthe.mp3"},
+                "JS":{"title":"Jai Sulthan","URL":"https://s089.naasongs.download/Sultan%20-%20(2021)/[iSongs.info]%2001%20-%20Jai%20Sultan.mp3"}
+            }
+         },
                "Tamil":
                 {
                     "Beast":{
-                            "ArabicKuthu":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Arabic%20Kuthu.mp3",
-                            "JollyOGymkhana":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Jolly%20O%20Gymkhana.mp3",
-                            "BeastMode":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Beast%20Mode.mp3",
+                            "AK":{"title":"ArabicKuthu","URL":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Arabic%20Kuthu.mp3"},
+                            "JOG":{"title":"JollyOGymkhana","URL":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Jolly%20O%20Gymkhana.mp3"},
+                            "BM":{"title":"BeastMode","URL":"https://mp3teluguwap.net/mp3/2022/Beast/Best%20-%20HQ/Beast%20Mode.mp3"},
                             },
                    "Kanchana":{
-                            "KarrupuPerazhaga":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Karuppu%20Perazhaga.mp3",
-                            "NilluNillu":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Nillu%20Nillu%20Nillu%20Nillu.mp3",
-                            "SangliBungli":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Sangili%20Bungili%20-%20Masstamilan.in.mp3",
-                            "KodiyavaninKathya":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Kodiyavanin%20Kathaya.mp3"
+                            "KP":{"title":"KarrupuPerazhaga","URL":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Karuppu%20Perazhaga.mp3"},
+                            "NN":{"title":"NilluNillu","URL":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Nillu%20Nillu%20Nillu%20Nillu.mp3"},
+                            "SB":{"title":"SangliBungli","URL":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Sangili%20Bungili%20-%20Masstamilan.in.mp3"},
+                            "KK":{"title":"KodiyavaninKathya","URL":"https://naasongs.vip/myuploads/uploads/Kanchana%20/Kodiyavanin%20Kathaya.mp3"}
                            },
                   "Enthiran":{
-                            "ArimaArima":"https://naasongs.vip/links/uploads/Enthiran%2520/Arima-Arima.mp3",
-                            "BoomBoomRoboDa":"https://naasongs.vip/links/uploads/Enthiran%2520/Boom-Boom-Robo-Da.mp3"
+                            "AA":{"title":"ArimaArima","URL":"https://naasongs.vip/links/uploads/Enthiran%2520/Arima-Arima.mp3"},
+                            "BBRD":{"title":"BoomBoomRoboDa","URL":"https://naasongs.vip/links/uploads/Enthiran%2520/Boom-Boom-Robo-Da.mp3"}
                           },
                   "KGF1":{
-                            "DheeraDheera":"https://friendstamilmp3.net/Filesystem/A-Z%20Movie%20Songs/KGF%20Chapter%201/Agni%20Nenjil%20(Dheera%20Dheera%20Minnal%20).mp3"
+                            "DD":{"title":"DheeraDheera","URL":"https://friendstamilmp3.net/Filesystem/A-Z%20Movie%20Songs/KGF%20Chapter%201/Agni%20Nenjil%20(Dheera%20Dheera%20Minnal%20).mp3"}
                          },
                   "KGF2":{
-                            "Toofan":"https://naasongs.vip/myuploads/uploads/KGF%202/Toofan%20%20Tamil.mp3"
+                            "TFN":{"title":"Toofan","URL":"https://naasongs.vip/myuploads/uploads/KGF%202/Toofan%20%20Tamil.mp3"}
                          },
                   "NaaneVaruvean":{
-                             "VeeraSoora":"https://pagalnew.com/download128/33103"
+                             "VS":{"title":"VeeraSoora","URL":"https://pagalnew.com/download128/33103"}
                                   }
          }
        }
