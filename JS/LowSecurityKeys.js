84386d19f1b181901a5a90c1247d03f5d34f2c88
sonpanabtej <hv2471920@gmail.com>
2024-07-21 22:19:32 +0530

    Update LowSecurityKeys.js
    Comments

--- JS/LowSecurityKeys.js
+++ JS/LowSecurityKeys.js
@@ -1,27 +1,31 @@
 class LowCrypt{
-    constructor(x,y){
+    constructor(x,y)
+    {
          this.x=x;
-          this.y=y;}
-r={0:'Q',1:'R',2:'S',3:'T',4:'U',5:'V',6:'W',7:'X',8:'Y',9:'Z'};
+         this.y=y;
+    }
+    r={0:'Q',1:'R',2:'S',3:'T',4:'U',5:'V',6:'W',7:'X',8:'Y',9:'Z'};
     ir={'Q':0,'R':1,'S':2,'T':3,'U':4,'V':5,'W':6,'X':7,'Y':8,'Z':9};
-     HC_1=(h)=>parseInt(h,2**4)+31;
-      CC_1=(f)=>String.fromCharCode(f);
-       SS_1=(g,c=',')=>String(g).split(c).join('');
-     ___dhash(x){
+    HC_1=(h)=>parseInt(h,2**4)+31;
+    CC_1=(f)=>String.fromCharCode(f);
+    SS_1=(g,c=',')=>String(g).split(c).join('');
+    ___dhash(x)
+   {
          var N=[];
-           for(var t in x){
+         for(var t in x)
+         {
                 N.push((x.charCodeAt(t)-(2**5-1)).toString(16));
          }
        return this.SS_1(N).toUpperCase();
        
-      }
-      
-      ___drhash(t){
-       
-            var ch=[];
-             for(var n=0;n<t.length/2;n++){
+   }
+   ___drhash(t)
+  {
+      var ch=[];
+      for(var n=0;n<t.length/2;n++)
+      {
                  ch.push(t.slice(n*2,(n+1)*2));
-            }
+      }
             return this.SS_1(ch.map(this.HC_1).map(this.CC_1));
       }
        ___dProductKey(f){
@@ -43,7 +47,7 @@ ___dRProductKey(g){
 }
 }
 //Low Security Key Genrate.java
-h=new LowCrypt;
+/*h=new LowCrypt;
 Vh=(x)=>{//Genratw
 re=btoa(x);
 return h.___dhash(re);
@@ -57,4 +61,4 @@ RunHash=(j)=>{
 s=h.___drhash(j);
 K=atob(s);
 eval(K);
-}
+}*/
