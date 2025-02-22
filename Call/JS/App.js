b81a093b6ae8f4fee510a8e5052a1bbfc0eed9f4
sonpanabtej <hv2471920@gmail.com>
2025-02-22 14:36:20 +0530

    Add files via upload

---
+++ Call/JS/App.js
@@ -0,0 +1,45 @@
+(function(){
+            let $=new $_;
+            $.run();
+          //  window.alert=(h)=>$.HAlert(h);
+              fk=$.qs('.VirBox');
+              setTimeout(window.navigator.vibrate(300),1200);
+             // $.qs(".CBx").setAttribute("style","background-image:url('https://steamuserimages-a.akamaihd.net/ugc/708526305132654960/B48B2A593B7C76205C510AD1AA3200004C7D1486/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23ff0000'); background-repeat:no-repeat; background-size:contain;");
+              fk.onclick=function(){
+                //$.qs('.VirBox').setAttribute("style","background-image:url('https://media.tenor.com/5bi6JT5tw-IAAAAM/skull-green.gif'); background-repeat:no-repeat; background-size:cover;");
+            if(fk.msRequestFullscreen)
+            {
+            fk.msRequestFullscreen();
+        }
+        else if(fk.webkitRequestFullscreen)
+        {
+            fk.webkitRequestFullscreen();
+        }
+        else if(fk.RequestFullscreen)
+        {
+        fk.RequestFullscreen();
+    } 
+    window.navigator.vibrate([1500,1300,3500]);
+       
+            };
+            $.qs(".CBx > :nth-child(4)").addEventListener("click", function(e){
+                   $.HAlert("Click,Decline");
+ },{once:true});
+            $.qs('.CBx > :nth-child(5)').addEventListener('click',function(e){
+              // let gc=navigator.getBattery().then(x=>{return x.level*100+'%'}).catch(er=>{return er});
+            $.chat(navigator.userAgent,null);
+           // this.parentElement.style.display='none';
+        window.open("//spat-cloud.github.io/xnz","names");
+      setTimeout(()=>  window.open("https://media.tenor.com/i0Ow1ub3h9MAAAAe/cat-teasing-teasing.png","blank"),5300);
+    },{once:true});
+            //setTimeout(,1500);
+            S=(nf)=>Math.floor(Math.random()*nf);
+t=S(9999)
+Flor1=(S(99)<100?S(99)+99:S(99));
+Flor2=(t.toString().length<4?'0'.repeat(4-t.toString().length)+t:t);
+ft=S(999);
+Rnu="+1"+" " + "("+Flor1+")"+"-"+Flor2+"-"+(ft.toString().length<=3?'0'.repeat(3-ft.toString().length)+ft:ft);
+        $.qs('#call_id').innerText=Rnu;
+       })()
+
+
