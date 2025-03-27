5e63750cd4a649856370f5648578a789ccb524bd
sonpanabtej <hv2471920@gmail.com>
2025-03-27 16:35:18 +0530

    Update HSDK.Private.Raw.js
    used Keywords var,let,const,Added Function prototype for String rCrypt converts Chars to Readable Random Stream of Other Langs

--- HarScript/HSDK.Private.Raw.js
+++ HarScript/HSDK.Private.Raw.js
@@ -9,11 +9,11 @@ class LowCrypt
           }
         
         
-          r={0:'Q',1:'R',2:'S',3:'T',4:'U',5:'V',6:'W',7:'X',8:'Y',9:'Z'};
-          ir={'Q':0,'R':1,'S':2,'T':3,'U':4,'V':5,'W':6,'X':7,'Y':8,'Z':9};
-          HC_1=(h)=>parseInt(h,2**4)+31;
-          CC_1=(f)=>String.fromCharCode(f);
-          SS_1=(g,c=',')=>String(g).split(c).join('');
+         var r={0:'Q',1:'R',2:'S',3:'T',4:'U',5:'V',6:'W',7:'X',8:'Y',9:'Z'};
+         var ir={'Q':0,'R':1,'S':2,'T':3,'U':4,'V':5,'W':6,'X':7,'Y':8,'Z':9};
+          let HC_1=(h)=>parseInt(h,2**4)+31;
+         let CC_1=(f)=>String.fromCharCode(f);
+         let SS_1=(g,c=',')=>String(g).split(c).join('');
             ___dhash()
           {
                 var N=[];
@@ -43,7 +43,7 @@ class HarScript extends LowCrypt
     {
         super(x);
     }
-    rh=()=>{
+    const rh=()=>{
       var np=Number.prototype,op=Object.prototype,sp=String.prototype;
       // np=;
       np.hex=function(){var hx=this.toString(16);return hx.length<6?'0x'+'0'.repeat(6-hx.length)+hx:'0x'+hx;};
@@ -57,7 +57,13 @@ class HarScript extends LowCrypt
       sp.ba=function(){return btoa(this);};
       sp.ab=function(){return atob(this);};
       sp.rev=function(){var t,c; t=this.split('');c=t.reverse();return c.toString().split(',').join('');};
-
+      sp.rCrypt=function(){
+                Regt=(2*8*32*128);
+             gs=(c)=>String.fromCharCode((Regt-3)-c.charCodeAt(0));
+             z=this.split('');
+             ma=z.map(gs);
+             return ma.toString().split(',').join("");
+       };
       sp.lh=function(){var xx=new LowCrypt(this.ba());return xx.___dhash().rev();}
       sp.hl=function(){var nn=new LowCrypt(this.rev());return nn.___drhash().ab();}
 
